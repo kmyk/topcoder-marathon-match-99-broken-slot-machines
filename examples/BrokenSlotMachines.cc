3cb6fd954beae1e60d99e8cab0d58c77fd1cc41e
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-03-16 16:19:23 +0900

    add examples

---
+++ examples/BrokenSlotMachines.cc
@@ -0,0 +1,11 @@
+#include <string>
+
+using namespace std;
+
+class BrokenSlotMachines {
+public:
+    int playSlots(int coins, int maxTime, int noteTime, int numMachines) {
+        PlaySlots::quickPlay(0, 1);
+        return 0;
+    }
+};
---
+++ examples/BrokenSlotMachines.py
@@ -0,0 +1,6 @@
+class BrokenSlotMachines:
+
+	def playSlots(self, coins, maxTime, noteTime, numMachines):
+        PlaySlots.quickPlay(0, 1)
+		return 0
+
