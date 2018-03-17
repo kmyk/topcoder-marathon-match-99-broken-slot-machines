cc79500351207ee69d10303d4ce285549ddd93ae
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-03-17 19:26:26 +0900

    update logger

--- foo.py
+++ foo.py
@@ -2,31 +2,35 @@
 # -*- coding: utf-8 -*-
 from colorama import Fore, Back, Style
 import json
-import pandas as pd
 import subprocess
+import sys
 
-def command_info(args):
-    columns = [ 'seed', 'coins', 'maxTime', 'noteTime', 'numMachines' ]
-    print(','.join(columns))
+def command_get(args):
+    data = []
     for seed in range(1, args.n + 1):
         proc = subprocess.run([ './a.out', '-json', '-seed', str(seed) ], stdout=subprocess.PIPE)
+        print(proc.stdout.decode(), file=sys.stderr)
         lines = proc.stdout.splitlines()
-        data = json.loads(lines[-1].decode())
-        print(','.join([ str(data[column]) for column in columns ]))
+        data += [ json.loads(lines[-1].decode()) ]
+    json.dump(data, sys.stdout)
 
 def command_diff(args):
-    df = pd.read_csv(args.info, index_col='seed')
-    df = pd.merge(df, pd.read_csv(args.a, index_col='seed').rename(columns={ 'result': 'a' }), left_index=True, right_index=True)
-    df = pd.merge(df, pd.read_csv(args.b, index_col='seed').rename(columns={ 'result': 'b' }), left_index=True, right_index=True)
+    with open(args.a) as fh:
+        a = json.load(fh)
+    with open(args.b) as fh:
+        b = json.load(fh)
+    assert len(a) == len(b)
     score_a = 0
     score_b = 0
-    for seed in df.index:
-        coins = df.loc[seed]['coins']
-        maxTime = df.loc[seed]['maxTime']
-        noteTime = df.loc[seed]['noteTime']
-        numMachines = df.loc[seed]['numMachines']
-        val_a = df.loc[seed]['a']
-        val_b = df.loc[seed]['b']
+    for i in range(len(a)):
+        assert a[i]['seed'] == b[i]['seed']
+        seed = a[i]['seed']
+        coins = a[i]['coins']
+        maxTime = a[i]['maxTime']
+        noteTime = a[i]['noteTime']
+        numMachines = a[i]['numMachines']
+        val_a = a[i]['result']
+        val_b = a[i]['result']
         score_a += val_a / max(1, val_a, val_b)
         score_b += val_b / max(1, val_a, val_b)
         if val_a > val_b:
@@ -54,18 +58,19 @@ def main():
     import argparse
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='command')
-    parser_info = subparsers.add_parser('info')
-    parser_info.add_argument('n', type=int)
+    parser_info = subparsers.add_parser('get')
+    parser_info.add_argument('-n', type=int, default=300)
     parser_diff = subparsers.add_parser('diff')
     parser_diff.add_argument('a')
     parser_diff.add_argument('b')
-    parser_diff.add_argument('--info', default='log/info.csv')
     args = parser.parse_args()
 
-    if args.command == 'info':
-        command_info(args)
+    if args.command == 'get':
+        command_get(args)
     elif args.command == 'diff':
         command_diff(args)
+    else:
+        parser.print_help()
 
 if __name__ == '__main__':
     main()
